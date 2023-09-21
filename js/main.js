a58ddffb79d49530aa7a4e0c41707e5b50cc4ec9
fkdkskdfjfk <yljw225@naver.com>
2023-09-21 15:03:07 +0900

    9/21 3시

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ new Swiper('.aiService .swiper', {
   loop: true, 
   autoplay: false, 
   slidesPerView: 4, // 한번에 보여줄 슬라이드 개수(기본값: 1)
-  spaceBetween: 30, // 슬라이드 사이 여백(간격) px
+  spaceBetween: 0, // 슬라이드 사이 여백(간격) px
   navigation: { // 슬라이드 이전/다음 버튼 사용
     nextEl: '.aiService .swiper-button-next', // 다음 버튼 요소
     prevEl: '.aiService .swiper-button-prev'// 다음 버튼 요소
