00081324bbca01d6e59707011cbebef2b9af01fe
fkdkskdfjfk <yljw225@naver.com>
2023-09-21 21:42:53 +0900

    9/21 업로드

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 // AI Service 슬라이드 기능
-new Swiper('.aiService .swiper', {
-  loop: true, 
-  autoplay: false, 
-  slidesPerView: 4, // 한번에 보여줄 슬라이드 개수(기본값: 1)
-  spaceBetween: 0, // 슬라이드 사이 여백(간격) px
-  navigation: { // 슬라이드 이전/다음 버튼 사용
-    nextEl: '.aiService .swiper-button-next', // 다음 버튼 요소
-    prevEl: '.aiService .swiper-button-prev'// 다음 버튼 요소
-  }
-});
+// new Swiper('.aiService .swiper', {
+//   loop: true, 
+//   autoplay: false, 
+//   slidesPerView: 4, // 한번에 보여줄 슬라이드 개수(기본값: 1)
+//   spaceBetween: 0, // 슬라이드 사이 여백(간격) px
+//   navigation: { // 슬라이드 이전/다음 버튼 사용
+//     nextEl: '.aiService .swiper-button-next', // 다음 버튼 요소
+//     prevEl: '.aiService .swiper-button-prev'// 다음 버튼 요소
+//   }
+// });
 
 // to-top
 const toTopEl = document.querySelector('#to-top');
@@ -19,7 +19,7 @@ toTopEl.addEventListener('click', function () {
 });
 
 window.addEventListener('scroll', function () {
-  if (this.window.scrollY > 500 ) {
+  if (window.scrollY > 500 ) {
     gsap.to(toTopEl, 0.6, {
       opacity: 1,
       x: 0
@@ -27,7 +27,19 @@ window.addEventListener('scroll', function () {
   } else {
     gsap.to(toTopEl, 0.6, {
       opacity: 0,
-      x:100
+      x: 100
     });
+  }
+});
+
+// AI Service 슬라이드 기능
+new Swiper('.aiService .swiper', {
+  loop: true, 
+  autoplay: false, 
+  slidesPerView: 4, // 한번에 보여줄 슬라이드 개수(기본값: 1)
+  spaceBetween: 0, // 슬라이드 사이 여백(간격) px
+  navigation: { // 슬라이드 이전/다음 버튼 사용
+    nextEl: '.aiService .swiper-button-next', // 다음 버튼 요소
+    prevEl: '.aiService .swiper-button-prev'// 다음 버튼 요소
   }
 });
