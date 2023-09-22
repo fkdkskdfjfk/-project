242dfcb53e9c1ff130065f3d5cdca289d76f2f62
fkdkskdfjfk <yljw225@naver.com>
2023-09-22 16:40:56 +0900

    error

--- js/main.js
+++ js/main.js
@@ -9,38 +9,35 @@ new Swiper('.aiService .swiper', {
     prevEl: '.aiService .swiper-button-prev'
   }
 });
-window.onload = function(){
   // to-top
-  const toTopEl = document.querySelector('#to-top');
-  toTopEl.addEventListener('click', function () {
-    gsap.to(window, 0.6, {
-      scrollTo: 0 
+const toTopEl = document.querySelector('#to-top');
+console.log(toTopEl);
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, 0.6, {
+    scrollTo: 0 
+  });
+});
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 500 ) {
+    gsap.to(toTopEl, 0.6, {
+      opacity: 1,
+      x: 0
     });
-  });
+  } else {
+    gsap.to(toTopEl, 0.6, {
+      opacity: 0,
+      x: 100
+    });
+  }
+});
 
-  window.addEventListener('scroll', function () {
-    if (window.scrollY > 500 ) {
-      gsap.to(toTopEl, 0.6, {
-        opacity: 1,
-        x: 0
-      });
-    } else {
-      gsap.to(toTopEl, 0.6, {
-        opacity: 0,
-        x: 100
-      });
-    }
-  });
-
-
-  const spyEls = document.querySelectorAll('section.scroll-spy');
-  spyEls.forEach(function (spyEl) {
-    new ScrollMagic.Scene({ // 감시할 장면(Scene) 추가 및 옵션 지정
-      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
-      triggerHook: 0.8 
-    })
-    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
-    .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
-  });
-
-}
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({ // 감시할 장면(Scene) 추가 및 옵션 지정
+    triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+    triggerHook: 0.8 
+  })
+  .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+  .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
+});
