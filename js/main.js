13e25041c9a4ab2e589949cb8de4265859297670
fkdkskdfjfk <yljw225@naver.com>
2023-09-26 21:36:26 +0900

    9/26 업로드

--- js/main.js
+++ js/main.js
@@ -83,17 +83,3 @@ hamburgerEl.addEventListener('click', function () {
 btn_closeEl.addEventListener('click', function () {
   bgEl.classList.remove('active');
 });
-
-// 미디어쿼리
-// if (matchMedia("screen and (max-width: 767px)").matches) {
-//   new Swiper('.aiService .swiper', {
-//     loop: true, 
-//     autoplay: false, 
-//     slidesPerView: 2, // 한번에 보여줄 슬라이드 개수(기본값: 1)
-//     spaceBetween: 0, // 슬라이드 사이 여백(간격) px
-//     navigation: { // 슬라이드 이전/다음 버튼 사용
-//       nextEl: '.aiService .swiper-button-next', 
-//       prevEl: '.aiService .swiper-button-prev'
-//     }
-//   });
-// }
